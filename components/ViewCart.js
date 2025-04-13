d0b8833ea30cee951df3546d20a9a8e58aea0e8c
Vivek Kushwaha <vivek89237@gmail.com>
2025-04-13 18:35:50 +0530

    cart

--- components/ViewCart.js
+++ components/ViewCart.js
@@ -63,141 +63,137 @@ const ViewCart = ({ VendorName, ContactNo }) => {
     }
   };
 
-  const openMap = () => {
-    navigation.navigate("MapScreen"); // Navigate to map screen
-  };
+  // const checkOut = () => (
+  //   <View
+  //     style={{
+  //       flex: 1,
+  //       justifyContent: "flex-end",
+  //       backgroundColor: "rgba(0,0,0,0.7)",
+  //     }}
+  //   >
+  //     <Pressable
+  //       onPress={() => setModal(false)}
+  //       style={{
+  //         alignItems: "center",
+  //         justifyContent: "center",
+  //       }}
+  //     >
+  //       <AntDesign
+  //         style={{ paddingBottom: 10 }}
+  //         name="closecircle"
+  //         size={34}
+  //         color="black"
+  //       />
+  //     </Pressable>
 
-  const checkOut = () => (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "flex-end",
-        backgroundColor: "rgba(0,0,0,0.7)",
-      }}
-    >
-      <Pressable
-        onPress={() => setModal(false)}
-        style={{
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <AntDesign
-          style={{ paddingBottom: 10 }}
-          name="closecircle"
-          size={34}
-          color="black"
-        />
-      </Pressable>
+  //     <View
+  //       style={{
+  //         backgroundColor: "white",
+  //         height: 500, // Increase height to accommodate new input
+  //         borderTopRightRadius: 10,
+  //         borderTopLeftRadius: 10,
+  //       }}
+  //     >
+  //       <Text
+  //         style={{
+  //           color: "black",
+  //           textAlign: "center",
+  //           paddingTop: 12,
+  //           fontSize: 17,
+  //           paddingBottom: 9,
+  //           borderBottomColor: "#C0C0C0",
+  //           borderBottomWidth: 0.8,
+  //         }}
+  //       >
+  //         {VendorName}
+  //       </Text>
+  //       <View
+  //         style={{
+  //           flexDirection: "row",
+  //           alignItems: "center",
+  //           borderBottomColor: "#C0C0C0",
+  //           borderBottomWidth: 0.8,
+  //           padding: 10,
+  //         }}
+  //       >
+  //         <MaterialIcons style={{}} name="timer" size={24} color="green" />
+  //         <Text
+  //           style={{
+  //             color: "black",
+  //             fontSize: 17,
+  //             fontWeight: "600",
+  //             marginLeft: 6,
+  //           }}
+  //         >
+  //           Delivery in 2 hour 30 mins
+  //         </Text>
+  //       </View>
+  //       <ScrollView>
+  //         {cart.map((item, index) => (
+  //           <FinalCheckout key={index} item={item} />
+  //         ))}
+  //         <View
+  //           style={{
+  //             borderBottomColor: "#D0D0D0",
+  //             borderBottomWidth: 1,
+  //           }}
+  //         />
+  //         <View
+  //           style={{
+  //             borderBottomColor: "#D0D0D0",
+  //             borderBottomWidth: 3,
+  //           }}
+  //         />
+  //       </ScrollView>
 
-      <View
-        style={{
-          backgroundColor: "white",
-          height: 500, // Increase height to accommodate new input
-          borderTopRightRadius: 10,
-          borderTopLeftRadius: 10,
-        }}
-      >
-        <Text
-          style={{
-            color: "black",
-            textAlign: "center",
-            paddingTop: 12,
-            fontSize: 17,
-            paddingBottom: 9,
-            borderBottomColor: "#C0C0C0",
-            borderBottomWidth: 0.8,
-          }}
-        >
-          {VendorName}
-        </Text>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            borderBottomColor: "#C0C0C0",
-            borderBottomWidth: 0.8,
-            padding: 10,
-          }}
-        >
-          <MaterialIcons style={{}} name="timer" size={24} color="green" />
-          <Text
-            style={{
-              color: "black",
-              fontSize: 17,
-              fontWeight: "600",
-              marginLeft: 6,
-            }}
-          >
-            Delivery in 2 hour 30 mins
-          </Text>
-        </View>
-        <ScrollView>
-          {cart.map((item, index) => (
-            <FinalCheckout key={index} item={item} />
-          ))}
-          <View
-            style={{
-              borderBottomColor: "#D0D0D0",
-              borderBottomWidth: 1,
-            }}
-          />
-          <View
-            style={{
-              borderBottomColor: "#D0D0D0",
-              borderBottomWidth: 3,
-            }}
-          />
-        </ScrollView>
+  //       {/* Grand Total Section */}
+  //       <View
+  //         style={{
+  //           flexDirection: "row",
+  //           alignItems: "center",
+  //           justifyContent: "space-between",
+  //           paddingVertical: 14,
+  //           padding: 10,
+  //           shadowColor: "#686868",
+  //           shadowOffset: { width: 0, height: 1 },
+  //           shadowOpacity: 1,
+  //           shadowRadius: 2,
+  //           elevation: 5,
+  //         }}
+  //       >
+  //         <Text
+  //           style={{
+  //             color: "#42E100", // Updated color
+  //             fontWeight: "bold",
+  //             paddingBottom: 3,
+  //             fontSize: 17,
+  //           }}
+  //         >
+  //           Grand Total
+  //         </Text>
+  //         <Text style={{ color: "#42E100", fontSize: 17, fontWeight: "600" }}>
+  //           {"₹"}{total}
+  //         </Text>
+  //       </View>
 
-        {/* Grand Total Section */}
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-            paddingVertical: 14,
-            padding: 10,
-            shadowColor: "#686868",
-            shadowOffset: { width: 0, height: 1 },
-            shadowOpacity: 1,
-            shadowRadius: 2,
-            elevation: 5,
-          }}
-        >
-          <Text
-            style={{
-              color: "#42E100", // Updated color
-              fontWeight: "bold",
-              paddingBottom: 3,
-              fontSize: 17,
-            }}
-          >
-            Grand Total
-          </Text>
-          <Text style={{ color: "#42E100", fontSize: 17, fontWeight: "600" }}>
-            {"₹"}{total}
-          </Text>
-        </View>
-
-        <TouchableOpacity
-          onPress={confirmAddress}
-          style={{
-            backgroundColor: "#42E100", // Updated color
-            padding: 10,
-            alignItems: "center",
-          }}
-          activeOpacity={0.9}
-        >
+  //       <TouchableOpacity
+  //         onPress={confirmAddress}
+  //         style={{
+  //           backgroundColor: "#42E100", // Updated color
+  //           padding: 10,
+  //           alignItems: "center",
+  //         }}
+  //         activeOpacity={0.9}
+  //       >
 
           
-          <Text style={{ color: "white", fontSize: 17, fontWeight: "700" }}>
-            Place Order
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+  //         <Text style={{ color: "white", fontSize: 17, fontWeight: "700" }}>
+  //           Place Order
+  //         </Text>
+  //       </TouchableOpacity>
+  //     </View>
+  //   </View>
+  // );
 
 
   return (
