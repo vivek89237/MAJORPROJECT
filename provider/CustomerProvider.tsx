141cfb7da0d573f09f2ce0a0d919798ae518df6a
Vinayak <vinu79998444@gmail.com>
2025-04-20 12:03:18 +0530

    updated address of customer

--- provider/CustomerProvider.tsx
+++ provider/CustomerProvider.tsx
@@ -1,29 +1,35 @@
-import {PropsWithChildren, createContext, useContext, useState, useEffect} from 'react';
-import {fetchCustomer} from "~/utils/Supabase";
+import { PropsWithChildren, createContext, useContext, useState, useEffect } from 'react';
+import { fetchCustomer } from "~/utils/Supabase";
 import { useAuth } from './AuthProvider';
 
 const CustomerContext = createContext({});
 
+export default function CustomerProvider({ children }: PropsWithChildren) {
+  const [customer, setCustomer] = useState({});
+  const { userId } = useAuth();
 
-export default function CustomerProvider ({children} : PropsWithChildren) {
-    const [customer, setCustomer] = useState({});
-    const {userId} = useAuth();
-    useEffect(()=>{
-      fetchCustomer(userId, setCustomer);
-    }, [userId, customer])
-    return (
-    <CustomerContext.Provider value ={{
-      setCustomer: setCustomer,
+  const refreshCustomer = async () => {
+    await fetchCustomer(userId, setCustomer);
+  };
+
+  useEffect(() => {
+    refreshCustomer();
+  }, [userId]);
+
+  return (
+    <CustomerContext.Provider value={{
+      setCustomer,
+      refreshCustomer, // 
       customerId: customer?.id,
-      customerName: customer?.Name, 
-      customerContact: customer?.ContactNo, 
-      customerAddress : customer?.Address,  
+      customerName: customer?.Name,
+      customerContact: customer?.ContactNo,
+      customerAddress: customer?.Address,
       customerImage: customer?.image,
-      customerCoordinates : [customer?.latitude, customer?.longitude],
-      }}>
-        {children}
+      customerCoordinates: [customer?.latitude, customer?.longitude],
+    }}>
+      {children}
     </CustomerContext.Provider>
-  )
+  );
 }
 
 export const useCustomer = () => useContext(CustomerContext);
